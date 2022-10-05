bcb7561c903d534a62ce0a93d655a943c396cc6e
Bruno Augusto <b.augusto1501@gmail.com>
2022-10-05 14:42:43 -0300

    Otimização imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.time.Period;
 
 public class Main {
     public static void main(String[] args) {
 
-        MapaAstral mapaAstral = new MapaAstral(LocalDateTime.of(1968, Month.DECEMBER, 16, 10, 10), "São Paulo");
+        MapaAstral mapaAstral = new MapaAstral(LocalDateTime.of(1968, Month.JANUARY, 16, 12, 10), "São Paulo");
         mapaAstral.getSol();
         mapaAstral.getAscendente();
         mapaAstral.getIdade();
